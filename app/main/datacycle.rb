0200112010a31309fefdc2c0875063c8b2fc1233
Sage Ross <ragesoss@gmail.com>
2015-12-06 10:05:39 -0800

    Change logs to puts

--- app/main/datacycle.rb
+++ app/main/datacycle.rb
@@ -1,7 +1,7 @@
 class Datacycle
   def self.keepgrabbing
     sleep 5
-    Rails.logger.info "Datacycle started: PID #{Process.pid}"
+    puts "Datacycle started: PID #{Process.pid}"
 
     i = 0
     loop do
@@ -17,18 +17,18 @@ class Datacycle
   end
 
   def self.cycle(i)
-    Rails.logger.info "starting cycle #{i}..."
+    puts "starting cycle #{i}..."
     articles = FindArticles.at_random(count: 10000)
-    Rails.logger.info "#{articles.count} mainspace articles found"
+    puts "#{articles.count} mainspace articles found"
     articles = DiscardRedirects.from(articles)
-    Rails.logger.info "#{articles.count} are not redirects"
+    puts "#{articles.count} are not redirects"
     articles = HighPageviews.from_among(articles, min_views: 300)
-    Rails.logger.info "#{articles.count} of those have high page views"
+    puts "#{articles.count} of those have high page views"
     articles = Ores.discard_high_revision_scores(articles, max_wp10: 30)
     if articles.count > 0
-      Rails.logger.info "#{articles.count} tweetable prospect(s) found!"
+      puts "#{articles.count} tweetable prospect(s) found!"
     else
-      Rails.logger.info "no tweetable articles found in that cycle"
+      puts "no tweetable articles found in that cycle"
     end
     Article.import articles
   end
