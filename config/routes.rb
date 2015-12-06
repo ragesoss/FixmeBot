826103937912d09c24ccdb32fcc0ce8343d0ad64
Sage Ross <ragesoss@gmail.com>
2015-12-05 22:25:23 -0800

    Move twitter meta tags to layout head

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get ':title' => 'article#show'
 
+  get '/' => 'article#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
