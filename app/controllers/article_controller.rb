f09d2001f12c2265c46ec184a31737efed5ae986
Sage Ross <ragesoss@gmail.com>
2015-12-05 18:00:04 -0800

    Start of twitter card view

---
+++ app/controllers/article_controller.rb
@@ -0,0 +1,7 @@
+class ArticleController < ApplicationController
+  def show
+    # title = params[:title].tr('_', ' ')
+    # @article = Article.find_by(title: title)
+    # render text: 'Not Found', status: 404 if @article.nil?
+  end
+end
