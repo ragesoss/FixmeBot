de7e31818bd151798dc63d1cdc1c65b9584c5593
Sage Ross <ragesoss@gmail.com>
2015-12-08 11:05:53 -0800

    Render pages for untweeted articles

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -2,6 +2,6 @@ class ArticleController < ApplicationController
   def show
     title = params[:title].tr('_', ' ')
     @article = Article.find_by(title: title)
-    render text: 'Not Found', status: 404 if @article.nil? || @article.tweeted != true
+    render text: 'Not Found', status: 404 if @article.nil?
   end
 end
