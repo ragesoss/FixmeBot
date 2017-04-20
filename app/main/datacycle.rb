1d21dff443fcafc04370925a4252fcdbfe97187a
Sage Ross <ragesoss@gmail.com>
2017-04-20 15:44:06 -0700

    Log less

--- app/main/datacycle.rb
+++ app/main/datacycle.rb
@@ -17,17 +17,17 @@ class Datacycle
   end
 
   def self.cycle(i)
-    Rails.logger.info "starting cycle #{i}..."
+    Rails.logger.debug "starting cycle #{i}..."
     articles = FindArticles.at_random(count: 10000)
-    Rails.logger.info "#{articles.count} mainspace articles found"
+    Rails.logger.debug "#{articles.count} mainspace articles found"
     articles = DiscardRedirects.from(articles)
-    Rails.logger.info "#{articles.count} are not redirects"
+    Rails.logger.debug "#{articles.count} are not redirects"
     articles = HighPageviews.from_among(articles, min_views: 300)
-    Rails.logger.info "#{articles.count} of those have high page views"
+    Rails.logger.debug "#{articles.count} of those have high page views"
     articles = Ores.discard_high_revision_scores(articles, max_wp10: 30)
-    Rails.logger.info "#{articles.count} of those have low revision scores"
+    Rails.logger.debug "#{articles.count} of those have low revision scores"
     articles = CategoryFilter.discard_disambiguation_pages(articles)
-    Rails.logger.info "#{articles.count} of those are not disambiguation pages"
+    Rails.logger.debug "#{articles.count} of those are not disambiguation pages"
     if articles.count > 0
       Rails.logger.info "#{articles.count} tweetable prospect(s) found!"
     else
